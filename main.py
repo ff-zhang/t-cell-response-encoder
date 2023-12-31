828a14ce08ea66542aed81e09c548ec2cca89b73
Felix Zhang <felixfzhang@hotmail.com>
2023-12-30 20:18:33 -0800

    add feedback and fix seed for PyTorch

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import os
-import glob
-
 import torch
 from torch.utils import data
 
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 from scripts import dataset, model, plot
@@ -29,26 +25,9 @@ LEVEL_VALUES = [
 
 params = {
     'max_epochs': 100,
-    'df': 'all'
+    'df': 'all',
+    'save': True,
 }
-
-
-def plot_loss(losses, title, **kwargs):
-    plt.figure(figsize=(9.6, 4.8))
-
-    for lr, loss in losses.items():
-        train_loss, val_loss = loss
-        plt.plot(train_loss, label='train: ' + str(lr))
-        plt.plot(val_loss, label='val.: ' + str(lr))
-
-    plt.title(f'Dataset(s) {title[0]} - {title[1]} Points')
-
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-
-    print(f'Saving loss to figure/nn-{kwargs["df"]}.png')
-    plt.savefig(f'figure/nn-{kwargs["df"]}.png')
 
 
 def MAPE_loss(input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
@@ -58,57 +37,41 @@ def MAPE_loss(input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     return torch.mean(torch.abs((target - input) / target))
 
 
-def train_model(filename=None, criterion=None, write=False):
+def train_model(datasets: list[data.Subset], filename=None, criterion=None, write=False):
     if write is True and filename is None:
-        raise FileExistsError
-
-    train_percent, valid_percent, test_percent = 0.7, 0.15, 0.15
-    learn_rates = [0.01, 0.001, 0.0001]
-
-    df = [f'PeptideComparison_{i}' for i in range(1, 10)] if params['df'] == 'all' else [f'PeptideComparison_{params["df"]}']
-    df = dataset.CytokineDataset(df)
-
-    train_num = int(train_percent * len(df))
-    valid_num = int(valid_percent * len(df))
-    test_num = len(df) - (train_num + valid_num)
+        raise FileNotFoundError
 
     if write:
         f = open(filename, 'w')
         f.close()
 
-    # input weight matrix: [ a_{11}, a_{12}, a_{13}, a_{14}, a_{15}, a_{16}
-    #                        a_{21}, a_{22}, a_{23}, a_{24}, a_{25}, a_{26} ]
-    # output weight matrix: [ b_{11}, b_{12}
-    #                         b_{21}, b_{22}
-    #                         b_{31}, b_{32}
-    #                         b_{41}, b_{42}
-    #                         b_{51}, b_{52} ]
-    train_set, val_set, test_set = data.random_split(df, [train_num, valid_num, test_num])
+    assert len(datasets) == 3
+    train_set, val_set, test_set = datasets
 
-    for _ in range(50):
+    print(f'Training on dataset {params["df"]} with {len(train_set)} points ({torch.seed()})\n')
+
+    learn_rates = [0.1, 0.01, 0.001]
+    for _ in range(1):
         train_loader = data.DataLoader(train_set, batch_size=1, shuffle=True)
         val_loader = data.DataLoader(val_set, batch_size=1, shuffle=True)
-        test_loader = data.DataLoader(test_set, batch_size=1, shuffle=True)
 
-        print(f'Iter. {_}: Training on dataset {params["df"]} with {train_num} points')
+        print(f'-------------------- Iter. {_} --------------------')
 
         losses = {}
 
         for lr in learn_rates:
-            nn = model.CytokineModel(6, 2, 5)
+            print(f'\tlearning rate : {lr}')
+            nn = model.CytokineModel()
 
-            if criterion == "MAPE":
+            if criterion == 'MAPE':
                 criterion = MAPE_loss
             else:
                 criterion = torch.nn.MSELoss('mean')
             optimizer = torch.optim.Adam(nn.parameters(), lr)
 
-            train_loss, val_loss = model.train(nn, train_loader, val_loader, criterion, optimizer, **params)
-            losses[lr] = [train_loss, val_loss]
-            torch.save(nn, 'model/test.pth')
-            # torch.save(nn, f'model/nn-{params["df"]}-{lr}.pth')
+            losses[lr] = model.train(nn, train_loader, val_loader, criterion, optimizer, **params)
 
-        plot_loss(losses, (params['df'], train_num), **params)
+        plot.plot_loss(losses, (params['df'], len(train_set)), **params)
 
         if write:
             with open(filename, 'a') as f:
@@ -116,79 +79,22 @@ def train_model(filename=None, criterion=None, write=False):
                 f.write(', '.join(str(w) for w in nn.fc2.weight.detach().numpy().flatten()) + '\n')
 
 
-def plot_weights(weights: str = 'out/weights.csv', file_format: str = 'pdf'):
-    df = pd.read_csv(weights)
-    X = list(range(1, df.shape[1] + 1))
-
-    # scatter plot showing average
-    plt.figure(figsize=(9.6, 4.8))
-    for row in df.iterrows():
-        plt.scatter(X, row[1], alpha=0.7, s=8)
-
-    plt.scatter(X, df.sum() / 22, color='black')
-
-    plt.title('Learned Weights on Dataset 1')
-    plt.xlabel('Weight')
-    plt.ylabel('Value')
-
-    plt.xticks(X)
-
-    # plt.show()
-    plt.savefig(f'figure/weights_all_scatter.{file_format}')
-
-    # box plot showing median, first and third quartile, interquartile, outliers
-    plt.figure(figsize=(9.6, 4.8))
-    plt.boxplot(df)
-
-    plt.title('Learned Weights on Dataset 1')
-    plt.xlabel('Weight')
-    plt.ylabel('Value')
-
-    # plt.show()
-    plt.savefig(f'figure/weights_all_box.{file_format}')
-
-    # error plot showing standard deviations
-    plt.figure(figsize=(9.6, 4.8))
-    avg = df.mean(0)
-    plt.errorbar(X, avg, [df.mean(0) - df.min(0), df.max(0) - avg], fmt='.k', ecolor='gray', lw=1, capsize=2)
-    plt.errorbar(X, avg, df.std(0), fmt='ok', lw=3, capsize=4)
-
-    plt.title('Learned Weights on Dataset 1')
-    plt.xlabel('Weight')
-    plt.ylabel('Value')
-
-    # plt.show()
-    plt.savefig(f'figure/weights_all_error.{file_format}')
-
-
-def model_predictions(file: str = 'model/nn-1-0.001.pth'):
-    model = torch.load(file)
-    model.eval()
-
-    f = open('out/predictions.csv', 'w')
-    print('target, prediction', file=f)
-
-    fig, axes = plt.subplots(nrows=8, ncols=1, figsize=(), sharex=True, sharey=True)
-
-    for n in range(2, 10):
-        df = [f for f in glob.glob(os.path.join('data/final', f'*PeptideComparison_{n}*.pkl'))]
-        df = dataset.CytokineDataset(df)
-        df = data.DataLoader(df, batch_size=1)
-
-    for i, n in enumerate([1, 10, 11, 100, 101]):
-        x, _, r = df[n]
-        target = r[:, -11].detach().numpy()
-        pred = model(x).squeeze().detach().numpy()
-
-            print(f'{target}, {pred}', file=f)
-
-            axes[n-2].plot(target, label="Target")
-            axes[n-2].plot(pred, label="Prediction")
-            # axes[n-2].legend()
-
-    plt.show()
-    f.close()
-
-
 if __name__ == '__main__':
+    # PyTorch seed for a nice training and testing dataset,
+    torch.manual_seed(5667615556836105505)
+
+    df = dataset.CytokineDataset([f'PeptideComparison_{i}' for i in range(1, 7)])
+    plot.plot_dataset(df, 'IL-17A')
+    for f in [f'PeptideComparison_{i}' for i in range(7, 10)]:
+        df = dataset.CytokineDataset([f])
+        plot.plot_dataset(df, 'IL-17A')
+
+    dfs = dataset.get_train_test_subset(params)
+    train_model(dfs)
+
+    # Load the manually saved trained model which trained using the fixed seed.
+    nn = torch.load('model/good-nn-0.01-all/eph-60.pth')
+    preds = model.evaluate(nn, data.DataLoader(dfs[0], batch_size=1, shuffle=True))
+    plot.plot_pred_concentration(preds, dfs[0])
+
     print('hello world!')
